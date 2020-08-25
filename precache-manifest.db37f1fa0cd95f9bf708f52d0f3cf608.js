780e51017c73bb6c54c7fd9a432cff11089f545f
root <root@DESKTOP-LC1EVA9.localdomain>
2020-08-25 21:58:44 +0800

    Updates

---
+++ precache-manifest.db37f1fa0cd95f9bf708f52d0f3cf608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb494bc4f289e1726a9b2c3289f6abc",
+    "url": "/coffeeshop-v2/index.html"
+  },
+  {
+    "revision": "8fe19e9cb3388181eecd",
+    "url": "/coffeeshop-v2/static/css/main.34fbadb2.chunk.css"
+  },
+  {
+    "revision": "b8c682bf356f0813fe5c",
+    "url": "/coffeeshop-v2/static/js/2.e85b5d42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coffeeshop-v2/static/js/2.e85b5d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe19e9cb3388181eecd",
+    "url": "/coffeeshop-v2/static/js/main.0188d746.chunk.js"
+  },
+  {
+    "revision": "dbb7a77055b4f44895b6",
+    "url": "/coffeeshop-v2/static/js/runtime-main.ec1ba67a.js"
+  }
+]);
